852f2c2cfecc92dbd5fb670b003c7ce722fef0cc
zozoheir <42655961+zozoheir@users.noreply.github.com>
2024-06-23 17:25:48 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='rumorz-sdk',
+      version='0.1',
+      description='Rumorz SDK for Python',
+      packages=find_packages(),
+      author='Othmane Zoheir',
+      author_email='othmane@rumorz.io',
+      url='',
+      )
